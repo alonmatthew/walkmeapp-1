876238c61d98226603f8301f63c0ee8f348ecd5c
alonmatthew <alonmatthew94@gmail.com>
2017-02-06 19:01:51 -0800

    sourced in react to client side

---
+++ client/public/js/app.js
@@ -0,0 +1,12 @@
+const Dashboard = React.createClass({
+  render: function() {
+    return(
+      <h1>react component</h1>
+    )
+  }
+})
+
+ReactDOM.render(
+  <Dashboard />,
+  document.getElementById('root')
+)
--- server/server.js
+++ server/server.js
@@ -1,14 +1,22 @@
 const
   express = require('express'),
   app = express(),
-  PORT = process.env.port || 3000,
   logger = require('morgan'),
   mongoose = require('mongoose'),
   bodyParser = require('body-parser'),
   passport = require('passport'),
   appRoutes = require('./routes/app.js'),
   ownerRoutes = require('./routes/owner.js'),
-  walkerRoutes = require('./routes/walker.js')
+  walkerRoutes = require('./routes/walker.js'),
+
+  PORT = process.env.port || 3000,
+  mongooseConnectionString = process.env.MONGODB_URL || 'mongodb://localhost/walkme-app'
+
+mongoose.connect(mongooseConnectionString, (err) => {
+  console.log(err || "Connected to MongoDB(walkme-app)")
+})
+
+app.use(express.static(process.env.PWD + '/client/public'))
 
 app.use('/', appRoutes)
 app.use('/owner', ownerRoutes)
