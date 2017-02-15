168e13560a35801a059ba7630a33bbae1b02639e
Joelle Fernandez <joellepfernandez@gmail.com>
2017-02-15 15:19:08 -0800

    added log out to home page

--- client/public/js/app.js
+++ client/public/js/app.js
@@ -64,11 +64,16 @@ const NavBar = React.createClass({
           navbar =
               <div className='text-center' style={{'marginTop':'1em','marginBottom':'1em' }}>
                 { user.local.owner ? (
-                    <a className="btn btn-primary btn-lg" href ="/walker/profile" role="button">Back to Profile</a> ) :
-                    ( <a className="btn btn-primary btn-lg" href="/owner/profile" role="button">Back to Profile</a>)
+                    <a className="btn btn-primary btn-lg" style={{'marginRight':'1em'}} href ="/walker/profile" role="button">Back to Profile</a> ) :
+                    ( <a className="btn btn-primary btn-lg" style={{'marginRight':'1em'}} href="/owner/profile" role="button">Back to Profile</a>)
                 }
-               </div>
-      } else {
+                 { user.local.owner ? (
+                     <a className="btn btn-primary btn-lg" href ="/walker/logout" role="button">Log Out</a> ) :
+                     ( <a className="btn btn-primary btn-lg" href="/owner/logout" role="button">Log Out</a>)
+                 }
+                </div>
+        } else {
+          navbar =
           navbar =
                 <div className='text-center' style={{'marginTop':'1em','marginBottom':'1em' }}>
 
