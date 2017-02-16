10369e18d354db1f509bbd50efd57d53b56aa133
Matthew Alon <alonmatthew94@gmail.com>
2017-02-15 19:32:38 -0800

    rendered walkers accepted walks to walks page

---
+++ client/public/js/walkerWalks.js
@@ -0,0 +1,60 @@
+const Dashboard = React.createClass({
+  getInitialState: function() {
+    return{
+      user: {
+        posts: []
+      }
+    }
+  },
+
+  componentWillMount: function() {
+    const statusRoute = '/walker/status'
+    const sendSearch = fetch(statusRoute, {credentials: 'same-origin'})
+
+    var self = this
+
+    function setUser(data) {
+      data.json().then((jsonData) => {
+        console.log(jsonData)
+        self.setState({
+          user: jsonData.user
+        })
+      })
+    }
+
+    sendSearch.then(setUser)
+
+  },
+
+  render: function() {
+    return(
+      <Post user={this.state.user} />
+    )
+  }
+})
+
+const Post = React.createClass({
+  render: function() {
+    const posts = this.props.user.posts.map((p) => {
+      return <div key={p._id}>
+              <h4>
+              {p.dog.name}<br/>
+              {p.date}<br/>
+              {p.owner.local.address}
+              </h4>
+            </div>
+
+    })
+    return (
+      <div>
+        <h1>Scheduled Walks</h1>
+        {posts}
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(
+  <Dashboard />,
+  document.getElementById('root')
+)
