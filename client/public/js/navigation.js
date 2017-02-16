df682de28026f8a65c0da9dd16b9a8edfe7f06d9
Joelle Fernandez <joellepfernandez@gmail.com>
2017-02-15 19:22:31 -0800

    added hamburger menu icon

--- client/public/js/navigation.js
+++ client/public/js/navigation.js
@@ -3,6 +3,7 @@ const NavBar = React.createClass({
     return(
       <nav className="">
         <ul id="sidebar" className="nav nav-stacked affix">
+          <li><a className="text-center"><i className="fa fa-bars fa-3x" aria-hidden="true"></i></a></li>
           <li>
             <a href="/" className="text-center"><i className="fa fa-home fa-3x" aria-hidden="true"></i>
             <p className="text-center">Home</p>
